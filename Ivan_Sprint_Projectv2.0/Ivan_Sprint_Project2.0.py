bd0ac1c63aeced10e24a9b8e84ff8b565e25b9a1
Ivanyouknowit <iku26_student@ivycollegiate.org>
2024-09-27 08:11:51 +0000

    Adds try-exception handler

--- Ivan_Sprint_Projectv2.0/Ivan_Sprint_Project2.0.py
+++ Ivan_Sprint_Projectv2.0/Ivan_Sprint_Project2.0.py
@@ -1,2 +1,12 @@
-input('What is your age?')
-print('That is a great age to be!')
+try:
+    input('What is your age?')
+    print()
+
+    dog = input ('What is your age?: ')
+    print(f'{dog}?! That is a great age to be!')
+
+    dog = int(input ('What is your age?: '))
+    print (f'(dog)?! That is a great age to be!')
+
+except ValueError:
+    print('Error That is not a valid number. ')
